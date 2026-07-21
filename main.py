1163aad7ec75f52e9e601820c38d23f6ea54dc9f
ElitonPenkeNot <eliton.penke@gmail.com>
2026-07-21 09:35:12 -0300

    adiciona arquivos principais do projeto: main.py, models.py e requirements.txt

---
+++ main.py
@@ -0,0 +1,12 @@
+#cd c:\users\elito\Project_beauty_parlor
+#python -m venv venv
+
+#ativar venv venv\Scripts\activate
+#venv\Scripts\activate
+#deactivate
+
+#liga o servidor FastAPI uvicorn main:app --reload
+# pip install -r requirements.txt
+
+from fastapi import FastAPI
+app= FastAPI()
---
+++ models.py
@@ -0,0 +1,137 @@
+from datetime import datetime,timedelta
+from sqlalchemy import create_engine,Column,String,Integer,Boolean,Float,ForeignKey,DateTime,Text,Date
+from sqlalchemy.orm import declarative_base,relationship
+
+db=create_engine("sqlite:///bd.db")
+
+base=declarative_base()
+
+
+# cliente
+class cliente(base):
+    __tablename__ = "clientes"
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    nome = Column("nome", String, nullable=False)
+    email = Column("email", String, unique=True, nullable=False)
+    telefone = Column("telefone", String, unique=True, nullable=False)
+    senha = Column("senha", String, nullable=False)
+    ativo = Column("ativo", Boolean, default=True)
+    admin = Column("admin", Boolean, default=False)
+    endereco = Column("endereco", String, nullable=False)
+    data_cadastro = Column("data_cadastro", DateTime, default=datetime.now)
+
+    def __init__(self, nome, email, senha, telefone,endereco,ativo=True,admin=False):
+        self.nome = nome
+        self.email = email
+        self.senha = senha
+        self.telefone = telefone
+        self.ativo = ativo
+        self.admin = admin
+        self.endereco = endereco
+
+
+# servico
+class servico(base):
+    __tablename__ = "servicos"
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    titulo = Column("titulo", String, nullable=False)
+    descricao = Column("descricao", Text, nullable=False)
+    preco = Column("preco", Float, nullable=False)
+    duracao_min = Column("duracao_min", Integer, nullable=False)
+    cor=Column("cor",String,nullable=True)
+    data_cadastro = Column("data_cadastro", DateTime, default=datetime.now)
+
+    def __init__(self, titulo, preco, duracao_min,cor, descricao=None):
+        self.titulo = titulo
+        self.preco = preco
+        self.duracao_min = duracao_min
+        self.cor=cor
+        self.descricao = descricao
+
+
+# agendamento
+class agendamento(base):
+    __tablename__ = "agendamentos"
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    id_cliente = Column("id_cliente", Integer, ForeignKey("clientes.id"), nullable=False)
+    data = Column("data", Date, nullable=False)
+    horario_inicio = Column("horario_inicio", DateTime, nullable=False)
+    horario_termino = Column("horario_termino", DateTime, nullable=True)  # calculado depois de somar os serviços
+    status = Column("status", String, default="pendente")
+    presenca_confirmada = Column("presenca_confirmada", Boolean, default=False)
+    observacao = Column("observacao", Text, nullable=True)
+
+    # relação com os serviços do agendamento (N:N via agendamento_servico, visto que um agendamento pode ter varios serviços em um so)
+    servicos = relationship("agendamento_servico", cascade="all,delete")
+
+    def __init__(self, id_cliente, data, horario_inicio,observacao ,status="pendente",horario_termino=None):
+        self.id_cliente = id_cliente
+        self.data = data
+        self.horario_inicio = horario_inicio
+        self.horario_termino = horario_termino
+        self.status = status
+        self.observacao = observacao
+
+    def calcular_termino(self):
+        #soma de todos os min dos items de serviços dentro do agendamento
+        total_min = sum(item.duracao_momento for item in self.servicos)
+        self.horario_termino = self.horario_inicio+ timedelta(minutes=total_min)
+        
+    def calcular_preco(self):
+        #soma o preco dos itme em servico
+        return sum(item.preco_momento for item in self.servicos)
+
+
+# agendamento_servico (tabela de junção entre um agendamente e um serviço em si == N:N)
+class agendamento_servico(base):
+    __tablename__ = "agendamento_servico"
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    id_agendamento = Column("id_agendamento", Integer, ForeignKey("agendamentos.id"), nullable=False)
+    id_servico = Column("id_servico", Integer, ForeignKey("servicos.id"), nullable=False)
+    preco_momento = Column("preco_momento", Float, nullable=False)
+    duracao_momento = Column("duracao_momento", Integer, nullable=False)
+
+    def __init__(self, id_agendamento, id_servico, preco_momento, duracao_momento):
+        self.id_agendamento = id_agendamento
+        self.id_servico = id_servico
+        self.preco_momento = preco_momento
+        self.duracao_momento = duracao_momento
+
+
+# notificacao
+class notificacao(base):
+    __tablename__ = "notificacoes"
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    id_agendamento = Column("id_agendamento", Integer, ForeignKey("agendamentos.id"), nullable=False)
+    tipo = Column("tipo", String, nullable=False)   
+    canal = Column("canal", String, nullable=False)   
+    enviado_em = Column("enviado_em", DateTime, default=datetime.now)
+
+    def __init__(self, id_agendamento, tipo, canal):
+        self.id_agendamento = id_agendamento
+        self.tipo = tipo
+        self.canal = canal
+
+
+class bloqueio(base):
+    __tablename__ = "bloqueios"
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    data = Column("data", Date, nullable=False)
+    dia_inteiro=Column("dia_inteiro",Boolean,default=True)
+    hora_inicio = Column("hora_inicio", DateTime, nullable=True) 
+    hora_fim = Column("hora_fim", DateTime, nullable=True)
+    motivo = Column("motivo", String, nullable=False)
+    criado_em = Column("criado_em", DateTime, default=datetime.now)
+
+    def __init__(self, data,dia_inteiro=True, hora_inicio=None, hora_fim=None, motivo=None):
+        self.data = data
+        self.hora_inicio = hora_inicio
+        self.hora_fim = hora_fim
+        self.motivo = motivo
+        self.dia_inteiro=dia_inteiro
